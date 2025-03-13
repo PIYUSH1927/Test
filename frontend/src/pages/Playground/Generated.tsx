3a273aa91a82064df210f8c5d85244b526cba85b
Piyush Padia <piyush.padia2021@vitstudent.ac.in>
2025-03-14 03:12:31 +0530

    changes

--- frontend/src/pages/Playground/Generated.tsx
+++ frontend/src/pages/Playground/Generated.tsx
@@ -240,7 +240,7 @@ export default function Generated() {
 
   const [selectedRoomId, setSelectedRoomId] = useState<string | null>(null);
 
-  const [scale, setScale] = useState(window.innerWidth < 786 ? 2 : 3.5);
+  const [scale, setScale] = useState(window.innerWidth < 786 ? 2.1 : 3.5);
   const floorPlanRef = useRef<HTMLDivElement>(null);
   const calculateBounds = () => {
     const allPoints = floorPlanData.rooms.flatMap((room) => room.floor_polygon);
